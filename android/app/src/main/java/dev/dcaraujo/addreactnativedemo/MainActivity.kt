d25dd29ed99a292e5fcf51c9a2082df253a74d2c
Christopher Araujo <78472544+dcaaraujo@users.noreply.github.com>
2022-11-01 22:51:51 +0200

    Added Android support for React Native
    Migrated to AndroidX

--- android/app/src/main/java/dev/dcaraujo/addreactnativedemo/MainActivity.kt
+++ android/app/src/main/java/dev/dcaraujo/addreactnativedemo/MainActivity.kt
@@ -1,11 +1,58 @@
 package dev.dcaraujo.addreactnativedemo
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import android.util.Log
+import android.widget.Button
 
 class MainActivity : AppCompatActivity() {
+    companion object {
+        const val OVERLAY_PERMISSION_REQ_CODE = 1994
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        bindButtonClickListener()
+        requestDrawOverlayPermission()
+    }
+
+    private fun bindButtonClickListener() {
+        val button = findViewById<Button>(R.id.show_react_native_screen_button)
+        button.setOnClickListener { showReactNativeScreen() }
+    }
+
+    private fun requestDrawOverlayPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
+                return
+            }
+            val intent = Intent(
+                Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package: $packageName")
+            )
+            startActivityForResult(intent, OVERLAY_PERMISSION_REQ_CODE)
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == OVERLAY_PERMISSION_REQ_CODE) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                if (!Settings.canDrawOverlays(this)) {
+                    Log.d("AddReactNativeDemo", "User did not grant permission")
+                }
+            }
+        }
+    }
+
+    private fun showReactNativeScreen() {
+        val intent = Intent(this, ReactNativeActivity::class.java)
+        intent.putExtra("componentName", "SharedReactNativeComponent")
+        startActivity(intent)
     }
 }
---
+++ android/app/src/main/java/dev/dcaraujo/addreactnativedemo/ReactNativeActivity.kt
@@ -0,0 +1,42 @@
+package dev.dcaraujo.addreactnativedemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.facebook.react.PackageList
+import com.facebook.react.ReactInstanceManager
+import com.facebook.react.ReactPackage
+import com.facebook.react.ReactRootView
+import com.facebook.react.common.LifecycleState
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler
+import com.facebook.soloader.SoLoader
+
+class ReactNativeActivity : AppCompatActivity(), DefaultHardwareBackBtnHandler {
+    private lateinit var reactRootView: ReactRootView
+    private lateinit var reactInstanceManager: ReactInstanceManager
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        SoLoader.init(this, false)
+        reactRootView = ReactRootView(this)
+        val packages: List<ReactPackage> = PackageList(application).packages
+        // Packages that cannot be autolinked yet can be added manually here, for example:
+        // packages.add(MyReactNativePackage())
+        // Remember to include them in `settings.gradle` and `app/build.gradle` too.
+        reactInstanceManager = ReactInstanceManager.builder()
+            .setApplication(application)
+            .setCurrentActivity(this)
+            .setBundleAssetName("index.android.bundle")
+            .setJSMainModulePath("index")
+            .addPackages(packages)
+            .setUseDeveloperSupport(BuildConfig.DEBUG)
+            .setInitialLifecycleState(LifecycleState.RESUMED)
+            .build()
+        val componentName = intent.getStringExtra("componentName")!!
+        reactRootView.startReactApplication(reactInstanceManager, componentName, null)
+        setContentView(reactRootView)
+    }
+
+    override fun invokeDefaultOnBackPressed() {
+        super.onBackPressed()
+    }
+}
