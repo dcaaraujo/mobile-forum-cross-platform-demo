76737951703a6d835edff48827819ee32d4a8687
Christopher Araujo <78472544+dcaaraujo@users.noreply.github.com>
2022-11-02 09:27:34 +0200

    Added Prettier as dev dependency to format file

--- react-native/index.js
+++ react-native/index.js
@@ -1,30 +1,40 @@
-import React from 'react';
-import { AppRegistry, Text, View, StyleSheet, Button, Alert } from 'react-native';
+import React from "react";
+import {
+  AppRegistry,
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  Alert,
+} from "react-native";
 
 const SharedReactNativeComponent = () => {
   const showNativeAlert = () => {
     Alert.alert("Native alert", "Shown from React Native!", [
       {
         text: "OK",
-        style: "cancel"
-      }
+        style: "cancel",
+      },
     ]);
-  }
+  };
   return (
     <View style={styles.container}>
       <Text>Here is a shared React Native view! 📱</Text>
-      <Button title='Show Alert' onPress={showNativeAlert} />
+      <Button title="Show Alert" onPress={showNativeAlert} />
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'lavender',
+    backgroundColor: "lavender",
     justifyContent: "center",
-    alignItems: "center"
-  }
+    alignItems: "center",
+  },
 });
 
-AppRegistry.registerComponent('SharedReactNativeComponent', () => SharedReactNativeComponent);
+AppRegistry.registerComponent(
+  "SharedReactNativeComponent",
+  () => SharedReactNativeComponent
+);
